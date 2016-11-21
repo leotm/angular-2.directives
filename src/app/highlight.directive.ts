fd1e30dce241020ed1ff9e801f08509b837ba911
LeoTM <leo.m@hotmail.co.uk>
2016-11-21 18:29:12 +0000

    Style with @Hostbinding and @HostListeners

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,13 +1,27 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer) {
+  constructor() {
     // this.elementRef.nativeElement.style.backgroundColor = 'green'; // This way sometimes fails, due to running in different environments
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green'); // Better way
+    // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green'); // Better way
+  }
+
+  private backgroundColor = 'white';
+
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = 'green';
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    this.backgroundColor = 'white';
   }
 
 }
