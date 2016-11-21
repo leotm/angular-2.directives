79272c05d4bc38450efdda13780dde9b8398760d
LeoTM <leo.m@hotmail.co.uk>
2016-11-21 20:27:36 +0000

    Custom directive TemplateRef & ViewContainerRef

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[dirUnless]'
 })
 export class UnlessDirective {
+  @Input() set dirUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
 
-  constructor() { }
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
 
 }
