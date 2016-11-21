fc7e1fde914bf53cf2a167c9825d6f10b888e218
LeoTM <leo.m@hotmail.co.uk>
2016-11-21 18:25:39 +0000

    Style with ElementRef & Renderer

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer) {
+    // this.elementRef.nativeElement.style.backgroundColor = 'green'; // This way sometimes fails, due to running in different environments
+    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green'); // Better way
+  }
 
 }
