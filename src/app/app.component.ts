018dbf5d97f92a6acb94e24211c2875f0326293d
LeoTM <leo.m@hotmail.co.uk>
2016-11-21 18:58:55 +0000

    Try *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dir works!';
+  private switch = true;
+  onSwitch () {
+    this.switch = false;
+  }
 }
